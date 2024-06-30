8b047ddb3e959ddfe23de42c79a2d6ccabe70042
Kannavi Mishra <79000269+cloudcreatr@users.noreply.github.com>
2024-06-30 08:32:15 +0000

    chore: Update TypeScript and Vite configurations

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,23 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
+import { unstable_defineAction } from "@remix-run/cloudflare";
+import { destroySession, getSession } from "./session";
+
+
+export const action = unstable_defineAction(async ({ request, response }) => {
+  const session = await getSession(request.headers.get("Cookie"))
+  const formdata = await request.formData()
+  const action = formdata.get("action")
+  if (action === "logout" && session.get("userId")) {
+    response.headers.set("Set-Cookie", await destroySession(session))
+    response.status = 301
+    response.headers.set("Location", "/auth")
+    return { status: 301 }
+  }
+
+  return { status: 401, message: "Unauthorized" }
+})
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -1,10 +1,39 @@
-import { Outlet } from "@remix-run/react";
+import { unstable_defineAction, unstable_defineLoader } from "@remix-run/cloudflare";
+import { Outlet, useFetcher } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
+
+import { destroySession, getSession } from "~/session";
+
+export const loader = unstable_defineLoader(async ({ request, response }) => {
+    const session = await getSession(request.headers.get("Cookie"))
+    if (!session.get("userId")) {
+        response.status = 401
+        response.headers.set("Location", "/auth")
+
+        return { status: 401, message: "Unauthorized" }
+    }
+    return { status: "success" }
+})
+
+
+function LogoutButton() {
+    const f = useFetcher()
+    return (
+        <f.Form method="post" action="/">
+            <input type="hidden" name="action" value="logout" />
+            <Button type="submit">Logout</Button>
+        </f.Form>
+    )
+}
 
 export default function Layout() {
     return (
         <div className="bg-red-500 text-white font-sans text-2xl">
             i am Layout component
+            <LogoutButton />
             <Outlet />
+          
         </div>
     )
 }
+
